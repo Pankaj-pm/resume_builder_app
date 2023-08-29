cf4396e4f2fb145f00d50e864812779dbc6866e9
red2 <pankaj.pmuchhadiya@gmail.com>
2023-08-29 08:52:56 +0530

    text controller and validation

--- lib/views/contact_info.dart
+++ lib/views/contact_info.dart
@@ -11,6 +11,15 @@ class ContactInfo extends StatefulWidget {
 
 class _ContactInfoState extends State<ContactInfo> {
   bool isFirst = true;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController address1Controller = TextEditingController();
+  TextEditingController address2Controller = TextEditingController();
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String name = "";
 
   @override
   Widget build(BuildContext context) {
@@ -96,126 +105,212 @@ class _ContactInfoState extends State<ContactInfo> {
           ),
           Expanded(
               child: IndexedStack(
-                index: isFirst ? 0 : 1,
-                children: [
-                  SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.all(21),
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.person,
-                                color: Colors.grey,
+            index: isFirst ? 0 : 1,
+            children: [
+              SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.all(21),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.person,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Expanded(
+                              child: TextFormField(
+                                controller: nameController,
+                                // onChanged: (value) {
+                                //   name = value;
+                                //   print(value);
+                                // },
+                                validator: (value) {
+                                  if (value?.isEmpty ?? false) {
+                                    return "Enter Your Name";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                decoration: InputDecoration(hintText: "Name", hintStyle: TextStyle(color: Colors.grey)),
                               ),
-                              SizedBox(
-                                width: 15,
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.mail,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(width: 15),
+                            Expanded(
+                                child: TextFormField(
+                              controller: emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value) {
+                                if (value?.isEmpty ?? false) {
+                                  return "Enter Email address";
+                                } else if (!value!.contains("@")) {
+                                  return "Enter Valid Email address";
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Colors.grey),
                               ),
-                              Expanded(
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      hintText: "Name", hintStyle: TextStyle(color: Colors.grey)),
+                            ))
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Icon(Icons.phone_android, color: Colors.grey),
+                            SizedBox(width: 15),
+                            Expanded(
+                              child: TextFormField(
+                                controller: phoneController,
+                                keyboardType: TextInputType.phone,
+                                validator: (value) {
+                                  if (value?.isEmpty ?? false) {
+                                    return "Enter Phone Number";
+                                  } else if (int.tryParse(value ?? "") == null) {
+                                    return "Enter Valid Phone Number";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  hintText: "Phone",
+                                  // prefix: Icon(Icons.phone_android),
+                                  // prefixIcon: Icon(Icons.phone_android),
+                                  hintStyle: TextStyle(color: Colors.grey),
                                 ),
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.mail,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15),
+                              child: Icon(
+                                Icons.location_on,
                                 color: Colors.grey,
                               ),
-                              SizedBox(width: 15),
-                              Expanded(
-                                  child: TextField(
-                                    keyboardType:TextInputType.emailAddress,
+                            ),
+                            SizedBox(width: 15),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  TextFormField(
+                                    controller: addressController,
+                                    keyboardType: TextInputType.streetAddress,
+                                    // obscureText: true,
+                                    // obscuringCharacter: "-",
                                     decoration: InputDecoration(
-                                      hintText: "Email",
+                                      hintText: "Address",
+                                      // prefix: Icon(Icons.phone_android),
+                                      // prefixIcon: Icon(Icons.phone_android),
                                       hintStyle: TextStyle(color: Colors.grey),
                                     ),
-                                  ))
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                          Row(
-                            children: [
-                              Icon(Icons.phone_android, color: Colors.grey),
-                              SizedBox(width: 15),
-                              Expanded(
-                                child: TextField(
-                                  keyboardType: TextInputType.phone,
-                                  decoration: InputDecoration(
-                                    hintText: "Phone",
-                                    // prefix: Icon(Icons.phone_android),
-                                    // prefixIcon: Icon(Icons.phone_android),
-                                    hintStyle: TextStyle(color: Colors.grey),
                                   ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 15),
-                                child: Icon(
-                                  Icons.location_on,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              SizedBox(width: 15),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    TextField(
-                                      keyboardType: TextInputType.streetAddress,
-                                      obscureText: true,
-                                      obscuringCharacter: "-",
-                                      decoration: InputDecoration(
-                                        hintText: "Address",
-                                        // prefix: Icon(Icons.phone_android),
-                                        // prefixIcon: Icon(Icons.phone_android),
-                                        hintStyle: TextStyle(color: Colors.grey),
-                                      ),
+                                  TextFormField(
+                                    controller: address1Controller,
+                                    decoration: InputDecoration(
+                                      hintText: "Address Line 1",
+                                      // prefix: Icon(Icons.phone_android),
+                                      // prefixIcon: Icon(Icons.phone_android),
+                                      hintStyle: TextStyle(color: Colors.grey),
                                     ),
-                                    TextField(
-                                      decoration: InputDecoration(
-                                        hintText: "Address Line 1",
-                                        // prefix: Icon(Icons.phone_android),
-                                        // prefixIcon: Icon(Icons.phone_android),
-                                        hintStyle: TextStyle(color: Colors.grey),
-                                      ),
+                                  ),
+                                  TextFormField(
+                                    controller: address2Controller,
+                                    decoration: InputDecoration(
+                                      hintText: "Address Line 2",
+                                      // prefix: Icon(Icons.phone_android),
+                                      // prefixIcon: Icon(Icons.phone_android),
+                                      hintStyle: TextStyle(color: Colors.grey),
                                     ),
-                                    TextField(
-                                      decoration: InputDecoration(
-                                        hintText: "Address Line 2",
-                                        // prefix: Icon(Icons.phone_android),
-                                        // prefixIcon: Icon(Icons.phone_android),
-                                        hintStyle: TextStyle(color: Colors.grey),
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {
+                                  if (formKey.currentState?.validate() ?? false) {
+                                    print("Save $name");
+                                  } else {
+                                    print("Invalid");
+                                  }
+                                },
+                                child: Text("Save")),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  print("Save");
+                                  nameController.clear();
+                                  emailController.clear();
+                                  phoneController.text = "";
+                                  addressController.text = "";
+                                  address1Controller.text = "";
+                                  address2Controller.text = "";
+                                },
+                                child: Text("Reset")),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  Container(
-                    child: CircleAvatar(
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.symmetric(vertical: 50),
+                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5)),
+                child: Stack(
+                  fit: StackFit.passthrough,
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundColor: Colors.grey,
+                      foregroundColor: Colors.black,
                       child: Text("Add"),
                     ),
-                  )
-                ],
-              ))
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      left: 80,
+                      child: Icon(Icons.add_circle_rounded),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ))
         ],
       ),
     );
